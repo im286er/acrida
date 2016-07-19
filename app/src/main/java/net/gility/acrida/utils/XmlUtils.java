7101ed89a6ec0e6c31cba42d4cbe48fca12d19ca
alimy <alimy@gility.net>
2016-07-19 15:54:04 +0800

    add XStreamConverter for retrofit2 but have a bug

--- app/src/main/java/net/gility/acrida/utils/XmlUtils.java
+++ app/src/main/java/net/gility/acrida/utils/XmlUtils.java
@@ -3,6 +3,7 @@ package net.gility.acrida.utils;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 import net.gility.acrida.model.AppException;
 
@@ -25,6 +26,8 @@ public class XmlUtils {
 
     private final static String TAG = XmlUtils.class.getSimpleName();
 
+    private final static HashMap<Class<?>, XStream> xStreamMap = new HashMap<>();
+
     /**
      * 将一个xml流转换为bean实体类
      * 
@@ -35,6 +38,7 @@ public class XmlUtils {
      */
     @SuppressWarnings("unchecked")
     public static <T> T toBean(Class<T> type, InputStream is) {
+        //*******************************************************
         XStream xmStream = new XStream(new DomDriver("UTF-8"));
         // 设置可忽略为在javabean类中定义的界面属性
         xmStream.ignoreUnknownElements();
@@ -43,9 +47,13 @@ public class XmlUtils {
         xmStream.registerConverter(new MyFloatCoverter());
         xmStream.registerConverter(new MyDoubleCoverter());
         xmStream.processAnnotations(type);
+        //*******************************************************/
+        //XStream xmStream = getXStream(type);
+        //XStreamHolder.xStream.processAnnotations(type);
         T obj = null;
         try {
-            obj = (T) xmStream.fromXML(is);
+             obj = (T) xmStream.fromXML(is);
+            // obj = (T) XStreamHolder.xStream.fromXML(is);
         } catch (Exception e) {
             TLog.log(TAG, "解析xml发生异常：" + e.getMessage());
         } finally {
@@ -63,6 +71,29 @@ public class XmlUtils {
     public static <T> T toBean(Class<T> type, byte[] bytes) {
         if (bytes == null) return null;
         return toBean(type, new ByteArrayInputStream(bytes));
+    }
+
+    private static XStream getXStream(Class<?> type) {
+        XStream xStream = xStreamMap.get(type);
+        if (xStream == null) {
+            xStream = newXStream(type);
+            xStreamMap.put(type, xStream);
+        }
+
+        return xStream;
+    }
+
+    private static XStream newXStream(Class<?> type) {
+        XStream xStream = new XStream(new DomDriver("UTF-8"));
+        // 设置可忽略为在javabean类中定义的界面属性
+        xStream.ignoreUnknownElements();
+        xStream.registerConverter(new MyIntCoverter());
+        xStream.registerConverter(new MyLongCoverter());
+        xStream.registerConverter(new MyFloatCoverter());
+        xStream.registerConverter(new MyDoubleCoverter());
+        xStream.processAnnotations(type);
+
+        return xStream;
     }
 
     private static class MyIntCoverter extends IntConverter {
